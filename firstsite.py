c60972b8040829ad44592099c2ca8243d199ecaf
Alexander Zakharov <djkein@yandex.ru>
2022-03-03 15:37:49 +0400

    Update firstsite.py

--- firstsite.py
+++ firstsite.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, flash, abort
+from FDataBase import FDataBase
 
 # Configs
 DATABASE = "/tmp/flsite.db"
@@ -27,3 +28,52 @@ def create_db():
     db.close()
 
 
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/')
+def index():
+    db = get_db()
+    # menu = get_mainmenu(db)
+    dbase = FDataBase(db)
+    return render_template('start.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbbase.add_Post(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка записи статьи на сайт!', category='error')
+            else:
+                flash('Статья успешно загружена!', category='succes')
+        else:
+            flash('Малое колличество символов в статье!', category='error')
+    return render_template('add_post.html', menu=dbbase.getMenu(), title='Добавление статьи')
+
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.GetPost(id_post)
+    if not title:
+        abort(404)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
